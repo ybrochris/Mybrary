4c7cad964d19dab76dce0a3be02983cc585ac39a
Chris Corby <chris3354@ntlworld.com>
2019-07-26 23:55:27 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 // const methodOverride = require('method-override')
 
 const indexRouter = require('./routes/index');
-// const authorRouter = require('./routes/authors')
+
+const authorRouter = require('./routes/authors');
 // const bookRouter = require('./routes/books')
 
 app.set('view engine', 'ejs');
@@ -19,7 +20,7 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 // app.use(methodOverride('_method'))
 app.use(express.static('public'));
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -27,8 +28,9 @@ const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
+
 app.use('/', indexRouter);
-// app.use('/authors', authorRouter)
+app.use('/authors', authorRouter)
 // app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000);
