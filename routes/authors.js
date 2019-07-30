1e140047814db435852bf6729cb178812a08d2e0
Chris Corby <chris3354@ntlworld.com>
2019-07-30 23:50:46 +0100

    Books/Create/Index/New

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const Author = require('../models/author');
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -10,21 +10,16 @@ router.get('/', async (req, res) => {
   }
   try {
     const authors = await Author.find(searchOptions)
-    res.render('authors/index', { 
-      authors: authors, 
+    res.render('authors/index', {
+      authors: authors,
       searchOptions: req.query
     })
   } catch {
     res.redirect('/')
-  }    
-});
-
-// New Author Route
-router.get('/new', (req, res) => {
-  res.render('authors/new', { author: new Author() })
+  }
 })
 
-// Create Author Route
+// New Author Route
 router.get('/new', (req, res) => {
   res.render('authors/new', { author: new Author() })
 })
@@ -44,6 +39,6 @@ router.post('/', async (req, res) => {
       errorMessage: 'Error creating Author'
     })
   }
-}) 
+})
 
 module.exports = router
